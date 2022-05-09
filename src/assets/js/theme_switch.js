0a515f07bf29d09c4d8394bb4e1b7fe20a31cd1e
Corentin Ducatez <corentin_ducatez@hotmail.fr>
2022-05-09 16:39:23 +0200

    Fix javascript for JS 2011 version (wkhtmltopdf qt patched restrictions)

--- src/assets/js/theme_switch.js
+++ src/assets/js/theme_switch.js
@@ -3,31 +3,33 @@
  * @summary: intervert between light/dark theme and save settings to local stroage.
  * Basically, replaces/toggles every CSS class that has '-light' class with '-dark' and opposite
  */
-function onToggleMode() {
-  let bg_light_list = document.querySelectorAll(".bg-light")
-  let bg_dark_list = document.querySelectorAll(".bg-dark")
-  let text_light_list = document.querySelectorAll(".text-light")
-  let text_dark_list = document.querySelectorAll(".text-dark")
+ function onToggleMode() {
+  var bg_light_list = document.querySelectorAll(".bg-light")
+  var bg_dark_list = document.querySelectorAll(".bg-dark")
+  var text_light_list = document.querySelectorAll(".text-light")
+  var text_dark_list = document.querySelectorAll(".text-dark")
 
-  bg_light_list.forEach((element) => {
-    element.className = element.className.replace(/bg-light/g, "bg-dark");
-  });
+  for (var index = 0; index < bg_light_list.length; index++)
+  {
+    bg_light_list[index].className = bg_light_list[index].className.replace(/bg-light/g, "bg-dark");
+  }
 
-  bg_dark_list.forEach((element) => {
-    element.className = element.className.replace(/bg-dark/g, "bg-light");
-  });
+  for (var index = 0; index < bg_dark_list.length; index++)
+  {
+    bg_dark_list[index].className = bg_dark_list[index].className.replace(/bg-dark/g, "bg-light");
+  }
 
-  text_light_list.forEach((element) => {
-    element.className = element.className.replace(/text-light/g, "text-dark");
-  });
+  for (var index = 0; index < text_light_list.length; index++)
+  {
+    text_light_list[index].className = text_light_list[index].className.replace(/text-light/g, "text-dark");
+  }
 
-  text_dark_list.forEach((element) => {
-    element.className = element.className.replace(/text-dark/g, "text-light");
-  });
+  for (var index = 0; index < text_dark_list.length; index++)
+  {
+    text_dark_list[index].className = text_dark_list[index].className.replace(/text-dark/g, "text-light");
+  }
 }
 
-window.addEventListener("load", function(){
-  // set in Dark mode by default
-  // by default it's in light mode
-  onToggleMode();
-});
+// set in Dark mode by default
+// by default it's in light mode
+// onToggleMode()
