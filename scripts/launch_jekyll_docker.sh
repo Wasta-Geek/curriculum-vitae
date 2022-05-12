27af741f3204f68c05ea44807d05b5d8f1a12c73
Corentin Ducatez <corentin_ducatez@hotmail.fr>
2022-05-12 10:22:43 +0200

    Fix URL by using absolute URL in local/github pages

--- scripts/launch_jekyll_docker.sh
+++ scripts/launch_jekyll_docker.sh
@@ -5,5 +5,5 @@ PARENT_DIR=$(builtin cd $SCRIPT_DIR/..; pwd)
 if [[ $# -gt 0 ]]; then
     docker run -it --rm -v "$PARENT_DIR/docs:/srv/jekyll" -p 4000:4000 -p 35729:35729 jekyll/jekyll $@
 else
-    docker run -it --rm -v "$PARENT_DIR/docs:/srv/jekyll" -p 4000:4000 -p 35729:35729 jekyll/jekyll jekyll serve --trace --force_polling --livereload --config _config_dev.yml
+    docker run -it --rm -v "$PARENT_DIR/docs:/srv/jekyll" -p 4000:4000 -p 35729:35729 jekyll/jekyll bash -c "JEKYLL_ENV=local jekyll serve --trace --force_polling --livereload --config _config_dev.yml"
 fi
