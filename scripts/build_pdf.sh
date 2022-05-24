e086648da45b907950bfbe9df33038d4c4155323
Corentin Ducatez <corentin_ducatez@hotmail.fr>
2022-05-24 16:36:45 +0200

    Remove user from docker (otherwise it prevent some github actions to works )

--- scripts/build_pdf.sh
+++ scripts/build_pdf.sh
@@ -4,4 +4,4 @@ PARENT_DIR=$(builtin cd $SCRIPT_DIR/..; pwd)
 
 docker build -t $IMAGE_NAME $PARENT_DIR
 
-docker run -it --rm  -v "$PARENT_DIR/docs/_site/:/home" $IMAGE_NAME bash
+docker run -it --rm  -v "$PARENT_DIR/docs/_site/:/home/_site" $IMAGE_NAME bash
