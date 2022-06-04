a33d51d40ecd5dea5d856391635d04d25d25c5d7
Corentin Ducatez <corentin_ducatez@hotmail.fr>
2022-06-04 09:26:06 +0200

    Try to build website

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,4 @@
 # To print the bucket's website URL after creation
 output "website_endpoint" {
-  value = aws_s3_bucket.main_bucket.bucket
-
+  value = aws_s3_bucket.main_bucket.website_endpoint
 }
