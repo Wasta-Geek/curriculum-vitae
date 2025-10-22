d1f8b0506773abcd63c999e9a716307f517156d5
Corentin Ducatez <corentin_ducatez@hotmail.fr>
2025-10-22 19:28:19 +0200

    Small scripts update

--- scripts/build_html.sh
+++ scripts/build_html.sh
@@ -3,4 +3,4 @@ CONTAINER_NAME="jekyll_build_html"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PARENT_DIR=$(builtin cd $SCRIPT_DIR/..; pwd)
 
-docker run -it --rm -v "$PARENT_DIR/src:/srv/jekyll" $IMAGE_NAME jekyll build --trace
+docker run -it --rm -v "$PARENT_DIR/docs:/srv/jekyll" $IMAGE_NAME jekyll build --trace -d /srv/jekyll/output/
